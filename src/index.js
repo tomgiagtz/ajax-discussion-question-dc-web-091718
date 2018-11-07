aee11592cc3adedd89415e992160b535d9f703f1
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-11-07 16:46:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to? undefined
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
